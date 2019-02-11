fa01d5884e61d82bf161ea1f60b3739dc7b04424
veredz <verose@live.com>
2019-02-11 17:56:57 +0200

    cosine similarity: dependency parser: run yap via RESTful API server instead of locally

--- word_embeddings/dependency_parser/generate_relevant_sets.py
+++ word_embeddings/dependency_parser/generate_relevant_sets.py
@@ -2,11 +2,12 @@ import json
 import optparse
 import os
 import string
-import subprocess
 import sys
 from collections import defaultdict
+from time import sleep
 
 import pandas as pd
+import requests
 from tqdm import tqdm
 
 DATA_DIR = os.path.join('..', 'data')
@@ -17,6 +18,61 @@ adjective_tags = ['JJ', 'JJR', 'JJS']
 verb_tags = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 adverb_tags = ['RB', 'RBR', 'RBS', 'RP']
 punctuation = string.punctuation.replace('-', '')
+
+
+def get_response(headers, data):
+    retry_timer = 1
+
+    while True:
+        try:
+            # response = requests.post('http://onlp.openu.org.il:8000/yap/heb/joint', headers=headers, data=data)
+            response = requests.post('http://localhost:8000/yap/heb/joint', headers=headers, data=data)
+            return response
+        except requests.exceptions.ConnectionError:
+            if retry_timer > 1000:
+                print('\nMore than 1000 seconds without getting a response! Exiting...')
+                exit(0)
+
+            sleep(retry_timer)
+            retry_timer *= 2
+
+
+def get_dependency_tree_for_sentence(sent):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    sentence = u"{}  ".format(sent)
+    data = {"text": sentence}
+    data = json.dumps(data)
+
+    response = get_response(headers, data)
+    tree = json.loads(response.text)
+    dep_tree = tree['dep_tree'].split('\n')
+    dep_dict = {}
+
+    for dep in dep_tree:
+        if not dep:
+            continue
+
+        dep = dep[:-5]  # remove trailing \r\t\t
+        dep = dep.split('\t')
+        num = dep[0]
+        word = dep[1]
+        lemma = dep[2]
+        tag = dep[3]
+        head = dep[6]
+        dependency = dep[7]
+
+        dep_dict[num] = {
+            "word": word,
+            "tag": tag,
+            "lemma": lemma,
+            "next": head,
+            "dep": dependency
+        }
+
+    return dep_dict
 
 
 def read_relevant_set(pos_tag, group):
@@ -31,99 +87,6 @@ def read_reference_set(pos_tag):
               encoding='utf-8') as f:
         relevant_set = json.load(f)
     return relevant_set
-
-
-def get_dependency_tree_for_sentence(sent, i, dataset=None):
-    sent = sent.translate(str.maketrans("", "", punctuation))
-    unique_name = '{}'.format(i) if not dataset else '{}_{}'.format(i, dataset)
-
-    while '  ' in sent:  # fix sentences
-        sent = sent.replace('  ', ' ')
-
-    with open(
-            os.path.join(OUTPUTS_DIR, 'tmp', 'sentence_{}.txt'.format(unique_name)),
-            'w',
-            encoding='utf-8',
-            newline='\n'
-    ) as f:
-        [f.write(u'{}\n'.format(word)) for word in sent.split(' ')]
-        f.write('\n')
-    go_workspace = os.environ['GOPATH']
-    yap_path = os.path.join(go_workspace, 'src', 'yap')
-    yap = os.path.join(yap_path, 'yap.exe' if os.name == 'nt' else 'yap')
-
-    hebma_cmd = [
-        yap,
-        "hebma",
-        "-raw",
-        os.path.join(OUTPUTS_DIR, 'tmp', 'sentence_{}.txt'.format(unique_name)),
-        "-out",
-        os.path.join(OUTPUTS_DIR, 'tmp', "output_{}.txt".format(unique_name))
-    ]
-    p = subprocess.Popen(hebma_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    p.communicate()
-
-    joint_cmd = [
-        yap,
-        "joint",
-        "-f",
-        os.path.join(yap_path, 'conf', "jointzeager.yaml"),
-        "-in",
-        os.path.join(OUTPUTS_DIR, 'tmp', "output_{}.txt".format(unique_name)),
-        "-l",
-        os.path.join(yap_path, 'conf', "hebtb.labels.conf"),
-        "-m",
-        os.path.join(yap_path, 'data', "joint_arc_zeager_model_temp_i33.b64"),
-        "-nolemma",
-        "-oc",
-        os.path.join(OUTPUTS_DIR, 'tmp', "output_dep_{}.txt".format(unique_name)),
-        "-om",
-        os.path.join(OUTPUTS_DIR, 'tmp', "output_om_{}.txt".format(unique_name)),
-        "-os",
-        os.path.join(OUTPUTS_DIR, 'tmp', "output_os_{}.txt".format(unique_name)),
-        "-jointstr",
-        "ArcGreedy",
-        "-oraclestr",
-        "ArcGreedy"
-    ]
-    p = subprocess.Popen(joint_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    p.communicate()
-
-    with open(
-            os.path.join(OUTPUTS_DIR, 'tmp', 'output_dep_{}.txt'.format('{}'.format(unique_name))),
-            encoding='utf-8'
-    ) as f:
-        dep_tree = f.read()
-
-    dep_tree = dep_tree.split('\t_\t_\n')  # every line ends with \t_\t_\n
-    dep_dict = {}
-
-    for dep in dep_tree:
-        if not dep:
-            continue
-
-        dep = dep.replace('\n', '')
-
-        if not dep:
-            continue
-
-        dep = dep.split('\t')
-        num = dep[0]
-        lemma = dep[2]
-        word = dep[1]
-        tag = dep[3]
-        head = dep[6]
-        dependency = dep[7]
-
-        dep_dict[num] = {
-            "word": word,
-            "lemma": lemma,
-            "tag": tag,
-            "next": head,
-            "dep": dependency
-        }
-
-    return dep_dict
 
 
 def repair_document(sentence):
@@ -165,7 +128,7 @@ def repair_document(sentence):
     return sentence
 
 
-def get_relevant_set(data, i):
+def get_relevant_set(data):
     relevant_set_nouns = defaultdict(list)
     relevant_set_verbs = defaultdict(list)
 
@@ -180,7 +143,7 @@ def get_relevant_set(data, i):
                 if not sentence:
                     continue
 
-                dep_tree = get_dependency_tree_for_sentence(sentence, i)
+                dep_tree = get_dependency_tree_for_sentence(sentence)
 
                 for dependency in dep_tree.values():
                     if dependency['next'] == '0':
@@ -196,13 +159,13 @@ def get_relevant_set(data, i):
                     elif curr_tag in adverb_tags and next_tag in verb_tags:
                         relevant_set_verbs[next_word].append(curr_word)
 
-    with open(os.path.join(OUTPUTS_DIR, 'relevant_set_nouns_{}.json'.format(i)), 'w', encoding='utf-8') as out_file:
+    with open(os.path.join(OUTPUTS_DIR, 'relevant_set_nouns.json'), 'w', encoding='utf-8') as out_file:
         json.dump(relevant_set_nouns, out_file, ensure_ascii=False)
-    with open(os.path.join(OUTPUTS_DIR, 'relevant_set_verbs_{}.json'.format(i)), 'w', encoding='utf-8') as out_file:
+    with open(os.path.join(OUTPUTS_DIR, 'relevant_set_verbs.json'), 'w', encoding='utf-8') as out_file:
         json.dump(relevant_set_verbs, out_file, ensure_ascii=False)
 
 
-def get_reference_set(data, i, dataset):
+def get_reference_set(data, dataset):
     dataset = dataset.replace('/', '_') if dataset else None  # support nested folders
     control_nouns = read_relevant_set('nouns', 'control')
     control_verbs = read_relevant_set('verbs', 'control')
@@ -225,7 +188,7 @@ def get_reference_set(data, i, dataset):
                 continue
 
             sentence = repair_document(sentence)
-            dep_tree = get_dependency_tree_for_sentence(sentence, i, dataset)
+            dep_tree = get_dependency_tree_for_sentence(sentence)
 
             for dependency in dep_tree.values():
                 if dependency['next'] == '0':
@@ -245,13 +208,13 @@ def get_reference_set(data, i, dataset):
                     reference_set_verbs[next_word].append(curr_word)
 
     with open(
-            os.path.join(OUTPUTS_DIR, 'reference_set_nouns_{}_{}.json'.format(i, dataset)),
+            os.path.join(OUTPUTS_DIR, 'reference_set_nouns_{}.json'.format(dataset)),
             'w',
             encoding='utf-8'
     ) as out_file:
         json.dump(reference_set_nouns, out_file, ensure_ascii=False)
     with open(
-            os.path.join(OUTPUTS_DIR, 'reference_set_verbs_{}_{}.json'.format(i, dataset)),
+            os.path.join(OUTPUTS_DIR, 'reference_set_verbs_{}.json'.format(dataset)),
             'w',
             encoding='utf-8'
     ) as out_file:
@@ -261,31 +224,21 @@ def get_reference_set(data, i, dataset):
 if __name__ == '__main__':
     # slice data[i*(len//k):(i+1)*(len//k)]
     parser = optparse.OptionParser()
-    parser.add_option('-i', action="store", type="int", help="job (block) index")
-    parser.add_option('-k', action="store", type="int", help="jobs amount")
     parser.add_option('--set_type', action="store", choices=['relevant', 'reference'])
     parser.add_option('--folder', action="store", default="doctors_articles")
     options, remainder = parser.parse_args()
 
-    block = options.i
-    jobs = options.k
-
     if not os.path.exists(os.path.join(OUTPUTS_DIR, 'tmp')):
         os.makedirs(os.path.join(OUTPUTS_DIR, 'tmp'))
-
-    start = lambda data: block * (len(data) // jobs)
-    end = lambda data: (block + 1) * (len(data) // jobs)
 
     if options.set_type == 'relevant':
         df = pd.read_csv(os.path.join(DATA_DIR, 'all_data.csv'))
         df = df.iloc[:, 2:]  # ignore first 2 columns
-        df = df.iloc[start(df):end(df)]
 
-        get_relevant_set(df, block)
+        get_relevant_set(df)
     elif options.set_type == 'reference':
         articles_path = os.path.join(DATA_DIR, options.folder)
         articles = os.listdir(articles_path)
         articles = [os.path.join(articles_path, article) for article in articles]
-        articles = articles[start(articles):end(articles)]
 
-        get_reference_set(articles, block, options.folder)
+        get_reference_set(articles, options.folder)
