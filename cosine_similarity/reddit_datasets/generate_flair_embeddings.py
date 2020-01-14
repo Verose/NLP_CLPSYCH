2d3c6c29097d076cb9279f0dd45b1073c7122684
veredz <verose@live.com>
2020-01-14 16:31:49 +0200

    Clean flair embeddings generation: no need for locks, input directory is expected to be filtered already. doesn't skip users with more than 500 posts.

--- cosine_similarity/reddit_datasets/generate_flair_embeddings.py
+++ cosine_similarity/reddit_datasets/generate_flair_embeddings.py
@@ -14,43 +14,31 @@ from common.utils import DATA_DIR
 logging.getLogger('smart_open').setLevel(logging.ERROR)
 
 
-THRESHOLD = 500
-controls_counter = None
-depressions_counter = None
-
-
 def init(*args):
-    """ store the counters for later use """
-    global controls_counter
-    global depressions_counter
-    controls_counter = args[0]
-    depressions_counter = args[1]
+    """ store the dataset for later use """
+    global dataset
+    dataset = args[0]
 
 
 def save_json(jfile):
-    global controls_counter
-    global depressions_counter
+    global dataset
 
     user_id = os.path.basename(jfile).split('.')[0]
+    save_path = os.path.join('..', DATA_DIR, 'pos_tags_{}_embeds_nolimit'.format(dataset), '{}.json'.format(user_id))
+
+    if os.path.isfile(save_path):
+        print("Skipping user {} file already exists".format(user_id))
+        return
+
     document_embeddings = get_doc_embeddings()
 
     with open(jfile, encoding='utf-8') as f:
         user_data = json.load(f)
-        label = user_data['label']
-
-        if label == 'control' and controls_counter.value >= THRESHOLD:
-            return
 
         if len(user_data['tokens']) > 500:
-            print('Skipping file {}.json'.format(user_id))
-            return
+            print('User {}.json has {} posts NOT skipping'.format(user_id, len(user_data['tokens'])))
+            # return
 
-        if label == 'control':
-            with controls_counter.get_lock():
-                controls_counter.value += 1
-        else:
-            with depressions_counter.get_lock():
-                depressions_counter.value += 1
         posts_list = user_data['tokens']  # each post is a list of tokens
         pos_tags_list = user_data['posTags']
         posts_lowercase_list = []
@@ -72,8 +60,7 @@ def save_json(jfile):
         user_data["posTags"] = pos_tags_list_lowercase
         user_data["embeddings"] = posts_embeddings_list
 
-        with open(os.path.join('..', DATA_DIR, 'pos_tags_{dataset}_embeds', '{user_id}.json'.format(
-                dataset=dataset, user_id=user_id)), 'w') as out_file:
+        with open(save_path, 'w') as out_file:
             json.dump(user_data, out_file)
         print('Finished with file {}.json'.format(user_id))
 
@@ -94,19 +81,17 @@ def get_doc_embeddings():
 if __name__ == "__main__":
     parser = optparse.OptionParser()
     parser.add_option('--dataset', choices=['rsdd', 'smhd'], default='rsdd', action="store")
+    parser.add_option('--n_processes', default=2, type=int, action="store")
     options, _ = parser.parse_args()
 
     dataset = options.dataset
     json_pattern = os.path.join('..', DATA_DIR, 'pos_tags_{}'.format(dataset), '*.json')
     json_files = [pos_json for pos_json in glob.glob(json_pattern) if pos_json.endswith('.json')]
 
-    controls_counter = Value('i', 0)
-    depressions_counter = Value('i', 0)
-
     pool = Pool(
-        processes=1,
+        processes=options.n_processes,
         initializer=init,
-        initargs=(controls_counter, depressions_counter, )
+        initargs=(dataset,)
     )
     results = pool.map(save_json, json_files)
     pool.close()
